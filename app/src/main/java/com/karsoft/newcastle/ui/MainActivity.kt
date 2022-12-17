8f83baca44e4adbe5e85c62b038b68b47442f99b
Diyarbek02 <diyarnsanbaev@gmail.com>
2022-12-17 12:13:53 +0500

    4-fragments ready!

--- app/src/main/java/com/karsoft/newcastle/ui/MainActivity.kt
+++ app/src/main/java/com/karsoft/newcastle/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.karsoft.newcastle
+package com.karsoft.newcastle.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.karsoft.newcastle.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
